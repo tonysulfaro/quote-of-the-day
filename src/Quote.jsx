a7112f73710fa3df1331054d499c146b598a2723
Tony Sulfaro <tony116523@gmail.com>
2020-03-17 01:05:05 -0400

    now looks nice

--- src/Quote.jsx
+++ src/Quote.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 
 const Quote = ({ quote }) => {
   return (
-    <div className="quote">
-      <p>
-        <strong>{quote.quote}</strong>
-      </p>
-      <p>
-        <i>{quote.author}</i>
-      </p>
+    <div
+      className="quote"
+      style={{ backgroundImage: `url(${quote.background})` }}
+    >
+      <div className="quoteContent">
+        <p>
+          <strong>{quote.quote}</strong>
+        </p>
+        <p>
+          <i>{quote.author}</i>
+        </p>
+      </div>
     </div>
   )
 }
