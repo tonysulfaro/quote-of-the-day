5a7254b1b47d2abafc9e84e8b3c4a0b21b7812df
Tony Sulfaro <tony116523@gmail.com>
2020-03-17 22:28:39 -0400

    Updates

---
+++ precache-manifest.33d682ee329e60b2612cd8fa07a9e027.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be84e8459db9108662de4bcff02c8a1",
+    "url": "/quote-of-the-day/index.html"
+  },
+  {
+    "revision": "958707e24148d8ec3eac",
+    "url": "/quote-of-the-day/static/css/main.76bc6ec4.chunk.css"
+  },
+  {
+    "revision": "31b7e459abb8ed230040",
+    "url": "/quote-of-the-day/static/js/2.ab3a55cd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/quote-of-the-day/static/js/2.ab3a55cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "958707e24148d8ec3eac",
+    "url": "/quote-of-the-day/static/js/main.0307e99e.chunk.js"
+  },
+  {
+    "revision": "835058f91a0aa41f2def",
+    "url": "/quote-of-the-day/static/js/runtime-main.c96dc15a.js"
+  }
+]);
